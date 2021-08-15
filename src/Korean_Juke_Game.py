e31dc9dbf5290ac9f3ef7a838f75216edb49c0b9
Dzemoro <dellorpl@gmail.com>
2021-08-15 19:45:19 +0200

    Changed textures of player and update of direction

--- src/Korean_Juke_Game.py
+++ src/Korean_Juke_Game.py
@@ -28,21 +28,28 @@ def draw_grid():
 class Player():
     def __init__(self, x, y):
         self.images_right = []
+        self.images_left = []
         self.index = 0
         self.counter = 0
         for num in range(2):
-            img = pygame.image.load('img/juke{num}.png')
-        self.image = pygame.transform.scale(img, (80, 40))
+            img_left = pygame.image.load(f'img/juke{num}.png')
+            img_left = pygame.transform.scale(img_left, (80, 40))
+            img_right = pygame.transform.flip(img_left, True, False)
+            self.images_right.append(img_right)
+            self.images_left.append(img_left)
+        #self.image = pygame.transform.scale(img, (80, 40))
+        self.image = self.images_left[self.index]
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
         self.vel_y = 0
         self.jumped = False
-
+        self.direction = 0
 
     def update(self):
         dx = 0
         dy = 0
+        walk_cooldown = 15
 
         #get key presses
         key = pygame.key.get_pressed()
@@ -53,9 +60,28 @@ class Player():
             self.jumped = False
         if key[pygame.K_LEFT]:
             dx -= 5
+            self.counter += 1
+            self.direction = -1
         if key[pygame.K_RIGHT]:
             dx += 5
+            self.counter += 1
+            self.direction = 1
+        if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+            self.counter = 0
+            self.index = 0
+            self.image = self.images_left[self.index]
         
+        #animation
+        if self.counter > walk_cooldown:
+            self.counter = 0
+            self.index += 1
+            if self.index >= len(self.images_left):
+                self.index = 0
+        if self.direction == 1:
+            self.image = self.images_right[self.index]
+        if self.direction == -1:
+            self.image = self.images_left[self.index]
+
         #gravity
         self.vel_y += 1
         if self.vel_y > 10:
@@ -141,7 +167,7 @@ while run:
 
     player.update()
 
-    draw_grid()
+    #draw_grid()
 
 
     for event in pygame.event.get():
